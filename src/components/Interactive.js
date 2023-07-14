2afb578a5379ac8ffbd1dfe62f1dc821fcb264fe
Derek Detweiler <derek.detweiler@gmail.com>
2023-07-13 20:43:46 -0400

    Fixed Interactive by removing some workarounds introduced to handle older Pixi interaction.

--- src/components/Interactive.js
+++ src/components/Interactive.js
@@ -1,20 +1,10 @@
 import {Circle, Polygon, Rectangle} from 'pixi.js';
-import AABB from '../AABB.js';
 import Data from '../Data.js';
 import createComponentClass from '../factory.js';
-import { arrayCache } from '../utils/array.js';
+import {arrayCache} from '../utils/array.js';
 
 const
-    getId = function (event) {
-        const
-            data = event.data,
-            originalEvent = data.originalEvent;
-
-        return originalEvent.type.substr(0, 5) + (data.identifier || (originalEvent.changedTouches && originalEvent.changedTouches[0] && originalEvent.changedTouches[0].identifier) || 0);
-    },
-    savedHitAreas = {}, //So generated hitAreas are reused across identical entities.
-    pointerInstances = {},
-    orphanPointers = [];
+    savedHitAreas = {}; //So generated hitAreas are reused across identical entities.
 
 export default createComponentClass(/** @lends platypus.components.Interactive.prototype */{
     id: 'Interactive',
@@ -96,7 +86,6 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
      * @memberof platypus.components
      * @uses platypus.Component
      * @constructs
-     * @listens platypus.Entity#camera-update
      * @listens platypus.Entity#dispatch-event
      * @listens platypus.Entity#handle-render
      * @listens platypus.Entity#input-off
@@ -114,8 +103,7 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
      * @fires platypus.Entity#pointercancel
      */
     initialize: function () {
-        this.pressed = false;
-        this.camera = AABB.setUp();
+        this.pressed = null;
         if (this.hitArea) {
             this.container.hitArea = this.setHitArea(this.hitArea);
         }
@@ -126,10 +114,6 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
     },
 
     events: {
-        "camera-update": function (camera) {
-            this.camera.set(camera.viewport);
-        },
-
         /**
          * This event dispatches a PIXI.Event on this component's PIXI.Sprite. Useful for rerouting mouse/keyboard events.
          *
@@ -152,12 +136,14 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
             }
         },
 
-        "pointerdown": function () {
-            this.pressed = true;
+        "pointerdown": function (event) {
+            if (this.pressed === null) { // so extra multi-touch presses won't trigger 'pressmove' / 'pressup'
+                this.pressed = event.pixiEvent.pointerId;
+            }
         },
 
         "pointermove": function (event) {
-            if (this.pressed && ((pointerInstances[getId(event.pixiEvent)] === this))) {
+            if (this.pressed === event.pixiEvent.pointerId) {
                 /**
                  * This event is triggered on press move (drag).
                  *
@@ -173,7 +159,7 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
         },
 
         "pointerup": function (event) {
-            if (this.pressed) {
+            if (this.pressed === event.pixiEvent.pointerId) {
                 /**
                  * This event is triggered on press up.
                  *
@@ -185,21 +171,21 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
                  * @param entity {platypus.Entity} The entity receiving this event.
                  */
                 this.owner.triggerEvent('pressup', event);
-                this.pressed = false;
+                this.pressed = null;
             }
         },
 
         "pointerupoutside": function (event) {
-            if (this.pressed) {
+            if (this.pressed === event.pixiEvent.pointerId) {
                 this.owner.triggerEvent('pressup', event);
-                this.pressed = false;
+                this.pressed = null;
             }
         },
 
         "pointercancel": function (event) {
-            if (this.pressed) {
+            if (this.pressed === event.pixiEvent.pointerId) {
                 this.owner.triggerEvent('pressup', event);
-                this.pressed = false;
+                this.pressed = null;
             }
         },
 
@@ -246,23 +232,6 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
                         sprite.removeListener(event, handler);
                     });
                 },
-                assignPointerTarget = (event) => { // May be able to remove this check with Pixi 7 input handling? - DDD
-                    const
-                        id = getId(event);
-
-                    if (pointerInstances[id] === this) {
-                        pointerInstances[id] = null;
-                        return this;
-                    } else if (orphanPointers.length) {
-                        const
-                            target = orphanPointers[orphanPointers.length - 1];
-
-                        orphanPointers.length -= 1;
-                        return target;
-                    } else {
-                        return pointerInstances[id] ?? null;
-                    }
-                },
                 trigger = (target, eventName, event) => {
                     const
                         {container} = target;
@@ -272,7 +241,8 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
                         event.data.originalEvent // This is a workaround for a bug in Pixi 3 where phantom hover events are triggered. - DDD 7/20/16
                         ) {
                         const
-                            {camera, owner, relativeToSelf} = target,
+                            {owner, relativeToSelf} = target,
+                            camera = owner.parent.worldCamera.viewport,
                             matrix = relativeToSelf ? container.transform.worldTransform : container.parent.transform.worldTransform,
                             msg = Data.setUp(
                                 "event", event.data.originalEvent,
@@ -291,14 +261,6 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
             sprite.eventMode = 'static';
             
             addListener('pointerdown', (event) => {
-                const
-                    id = getId(event);
-
-                if (pointerInstances[id]) { // Hmm, this is a shared identifer - not supposed to happen. We'll save for later to make sure it gets its "pointerup" event.
-                    orphanPointers.push(pointerInstances[id]);
-                }
-                pointerInstances[id] = this;
-
                 /**
                  * This event is triggered on pointer down.
                  *
@@ -313,72 +275,57 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
                 event.currentTarget.mouseTarget = true;
             });
             addListener('pointerup', (event) => {
-                const
-                    target = assignPointerTarget(event);
-
-                if (target) {
-                    /**
-                     * This event is triggered on pointer up.
-                     *
-                     * @event platypus.Entity#pointerup
-                     * @param event {DOMEvent} The original DOM pointer event.
-                     * @param pixiEvent {PIXI.interaction.InteractionEvent} The Pixi pointer event.
-                     * @param x {Number} The x coordinate in world units.
-                     * @param y {Number} The y coordinate in world units.
-                     * @param entity {platypus.Entity} The entity receiving this event.
-                     */
-                    trigger(target, 'pointerup', event);
-                    event.currentTarget.mouseTarget = false;
-                    
-                    if (event.currentTarget.removeDisplayObject) {
-                        event.currentTarget.removeDisplayObject();
-                    }
+                /**
+                 * This event is triggered on pointer up.
+                 *
+                 * @event platypus.Entity#pointerup
+                 * @param event {DOMEvent} The original DOM pointer event.
+                 * @param pixiEvent {PIXI.interaction.InteractionEvent} The Pixi pointer event.
+                 * @param x {Number} The x coordinate in world units.
+                 * @param y {Number} The y coordinate in world units.
+                 * @param entity {platypus.Entity} The entity receiving this event.
+                 */
+                trigger(this, 'pointerup', event);
+                event.currentTarget.mouseTarget = false;
+                
+                if (event.currentTarget.removeDisplayObject) {
+                    event.currentTarget.removeDisplayObject();
                 }
             });
             addListener('pointerupoutside', (event) => {
-                const
-                    target = assignPointerTarget(event);
-
-                if (target) {
-                    /**
-                     * This event is triggered on pointer up outside.
-                     *
-                     * @event platypus.Entity#pointerupoutside
-                     * @param event {DOMEvent} The original DOM pointer event.
-                     * @param pixiEvent {PIXI.interaction.InteractionEvent} The Pixi pointer event.
-                     * @param x {Number} The x coordinate in world units.
-                     * @param y {Number} The y coordinate in world units.
-                     * @param entity {platypus.Entity} The entity receiving this event.
-                     */
-                    trigger(target, 'pointerupoutside', event);
-                    event.currentTarget.mouseTarget = false;
-                    
-                    if (event.currentTarget.removeDisplayObject) {
-                        event.currentTarget.removeDisplayObject();
-                    }
+                /**
+                 * This event is triggered on pointer up outside.
+                 *
+                 * @event platypus.Entity#pointerupoutside
+                 * @param event {DOMEvent} The original DOM pointer event.
+                 * @param pixiEvent {PIXI.interaction.InteractionEvent} The Pixi pointer event.
+                 * @param x {Number} The x coordinate in world units.
+                 * @param y {Number} The y coordinate in world units.
+                 * @param entity {platypus.Entity} The entity receiving this event.
+                 */
+                trigger(this, 'pointerupoutside', event);
+                event.currentTarget.mouseTarget = false;
+                
+                if (event.currentTarget.removeDisplayObject) {
+                    event.currentTarget.removeDisplayObject();
                 }
             });
             addListener('pointercancel', (event) => {
-                const
-                    target = assignPointerTarget(event);
-
-                if (target) {
-                    /**
-                     * This event is triggered on pointer cancel.
-                     *
-                     * @event platypus.Entity#pointercancel
-                     * @param event {DOMEvent} The original DOM pointer event.
-                     * @param pixiEvent {PIXI.interaction.InteractionEvent} The Pixi pointer event.
-                     * @param x {Number} The x coordinate in world units.
-                     * @param y {Number} The y coordinate in world units.
-                     * @param entity {platypus.Entity} The entity receiving this event.
-                     */
-                    trigger(target, 'pointercancel', event);
-                    event.currentTarget.mouseTarget = false;
-                    
-                    if (event.currentTarget.removeDisplayObject) {
-                        event.currentTarget.removeDisplayObject();
-                    }
+                /**
+                 * This event is triggered on pointer cancel.
+                 *
+                 * @event platypus.Entity#pointercancel
+                 * @param event {DOMEvent} The original DOM pointer event.
+                 * @param pixiEvent {PIXI.interaction.InteractionEvent} The Pixi pointer event.
+                 * @param x {Number} The x coordinate in world units.
+                 * @param y {Number} The y coordinate in world units.
+                 * @param entity {platypus.Entity} The entity receiving this event.
+                 */
+                trigger(this, 'pointercancel', event);
+                event.currentTarget.mouseTarget = false;
+                
+                if (event.currentTarget.removeDisplayObject) {
+                    event.currentTarget.removeDisplayObject();
                 }
             });
             addListener('pointermove', (event) => {
@@ -439,19 +386,6 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
             }
 
             this.removeInputListeners = () => {
-                const
-                    keys = Object.keys(pointerInstances),
-                    {length} = keys;
-
-                for (let i = 0; i < length; i++) {
-                    const
-                        key = keys[i];
-                    
-                    if (pointerInstances[key] === this) {
-                        pointerInstances[key] = null;
-                    }
-                }
-
                 for (let i = 0; i < removals.length; i++) {
                     removals[i]();
                 }
@@ -487,13 +421,11 @@ export default createComponentClass(/** @lends platypus.components.Interactive.p
 
         destroy () {
             const
-                {camera, container, removeInputListeners} = this;
+                {container, removeInputListeners} = this;
 
             if (removeInputListeners) {
                 removeInputListeners();
             }
-
-            camera.recycle();
             
             // This handles removal after the mouseup event to prevent weird input behaviors. If it's not currently a mouse target, we let the render component handle its removal from the parent container.
             if (container.mouseTarget && container.parent) {
