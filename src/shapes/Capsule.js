befe8ef88ba7c269362803e16aa75a2d81b16ee1
Derek Detweiler <derek.detweiler@gmail.com>
2025-07-30 10:17:13 -0400

    Updated Capsule to accept width and height.

--- src/shapes/Capsule.js
+++ src/shapes/Capsule.js
@@ -1,6 +1,23 @@
 import adjustedXY from "./adjustedXY";
 import Circle from "./Circle";
+import Rectangle from "./Rectangle";
 import Segment from "./Segment";
+
+const
+    axisInfo = {
+        x: {
+            axis: 'x',
+            alt: 'y',
+            length: 'width',
+            breadth: 'height'
+        },
+        y: {
+            axis: 'y',
+            alt: 'x',
+            length: 'height',
+            breadth: 'width' 
+        }
+    };
 
 export default class Capsule extends Segment {
     constructor (...args) {
@@ -9,8 +26,29 @@ export default class Capsule extends Segment {
     }
 
     initialize (options) {
-        super.initialize(options);
-        this.radius = options.radius;
+        if (!options.points && (!options.a || !options.b) && options.width && options.height) {
+            const
+                {alt, axis, breadth, length} = axisInfo[options.width > options.height ? 'x' : 'y'],
+                radius = options[breadth] / 2,
+                len = options[length] - 2 * radius,
+                hl = len / 2;
+
+            super.initialize({
+                ...options,
+                a: {
+                    [axis]: -hl,
+                    [alt]: 0
+                },
+                b: {
+                    [axis]: hl,
+                    [alt]: 0
+                }
+            });
+            this.radius = radius;
+        } else {
+            super.initialize(options);
+            this.radius = options.radius;
+        }
     }
 
     duplicate () {
@@ -31,6 +69,6 @@ export default class Capsule extends Segment {
     }
 
     static validateDefinition (definition) {
-        return Circle.validateDefinition(definition) && Segment.validateDefinition(definition);
+        return Rectangle.validateDefinition(definition) || (Circle.validateDefinition(definition) && Segment.validateDefinition(definition));
     }
 }
